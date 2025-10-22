0167500d05c71d3d855107bf0ce7e18ab0bbf5d5
Phil Chacko <phil.chacko@gmail.com>
2025-10-22 16:30:52 -0700

    Reticle tweaks

--- src/components/hud/ClickToPlay.tsx
+++ src/components/hud/ClickToPlay.tsx
@@ -51,9 +51,9 @@ export function Reticle({ visible }: { visible: boolean }) {
   if (!visible) return null;
   return (
     <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 opacity-85">
-      <div className="relative w-[18px] h-[18px]">
-        <div className="absolute left-1/2 top-0 w-[2px] h-full -translate-x-1/2 bg-white/90" />
-        <div className="absolute top-1/2 left-0 h-[2px] w-full -translate-y-1/2 bg-white/90" />
+      <div className="relative w-[12px] h-[12px]">
+        <div className="absolute left-1/2 top-0 w-[2px] h-full -translate-x-1/2 bg-zinc-200/40" />
+        <div className="absolute top-1/2 left-0 h-[2px] w-full -translate-y-1/2 bg-zinc-200/40" />
       </div>
     </div>
   );
